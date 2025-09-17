e66824eca96d2b41cfd70fd1879650ae1ea5892e
Shredded-Pork <12421147@zju.edu.cn>
2025-09-17 16:25:44 +0800

    Add files via upload

---
+++ scripts/multi_node/main.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Common part for all nodes 
+# Launch command (parameters automatically read from accelerate_multi_node.yaml)
+accelerate launch --num_machines 6 --num_processes 48 \
+    --machine_rank ${RANK} --main_process_ip ${MASTER_ADDR} --main_process_port ${PORT} \
+    scripts/train_sd3.py \
+    --config config/dgx.py:geneval_sd3

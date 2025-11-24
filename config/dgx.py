63e4def7159940ba7d60e4e6250eee868342388c
Shredded-Pork <12421147@zju.edu.cn>
2025-11-24 15:33:12 +0800

    Update dgx.py

--- config/dgx.py
+++ config/dgx.py
@@ -354,6 +354,45 @@ def flux_pr():
     config.run_name = 'run name'
     return config
 
+def pickscore_qwenimage():
+    config = compressibility()
+    config.dataset = os.path.join(os.getcwd(), "dataset/pickscore")
+    config.mixed_precision = "bf16"
+
+    # flux
+    config.pretrained.model = "Qwen-Image"
+    config.sample.num_steps = 10
+    config.sample.eval_num_steps = 50
+    config.sample.guidance_scale = 4
+
+    config.resolution = 512
+    config.sample.train_batch_size = 6
+    config.sample.num_image_per_prompt = 24
+    config.sample.num_batches_per_epoch = 4
+    config.sample.test_batch_size = 4 # This bs is a special design, the test set has a total of 2048, to make gpu_num*bs*n as close as possible to 2048, because when the number of samples cannot be divided evenly by the number of cards, multi-card will fill the last batch to ensure each card has the same number of samples, affecting gradient synchronization.
+
+    config.train.batch_size = config.sample.train_batch_size
+    config.train.gradient_accumulation_steps = config.sample.num_batches_per_epoch//2
+    config.train.num_inner_epochs = 1
+    config.train.beta = 0
+    config.sample.global_std = False
+    config.train.ema = False
+    config.num_epochs = 250
+    config.save_freq = 30 # epoch
+    config.eval_freq = 20
+    config.save_dir = ''
+    config.reward_fn = {
+        "pickscore": 1.0,
+    }
+    
+    config.prompt_fn = "general_ocr"
+
+    config.per_prompt_stat_tracking = True
+    config.run_name = ''
+
+    return config
+
+
 def pickscore_qwenimage_pr():
     config = compressibility()
     config.dataset = os.path.join(os.getcwd(), "dataset/pickscore")
